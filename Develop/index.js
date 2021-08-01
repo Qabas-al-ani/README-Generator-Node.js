2f84c5f5b88d87966817a5c8bc459961d4bfe0bb
Qabas al ani <doctorqa@yahoo.com>
2021-08-01 15:38:25 -0400

    Declaring variables

---
+++ Develop/index.js
@@ -0,0 +1,7 @@
+// declaring variables with their requirements 
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
+
